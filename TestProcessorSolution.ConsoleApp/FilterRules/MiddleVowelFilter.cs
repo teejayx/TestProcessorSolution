60398539394c800284ff9d171acf3ce07d0e2d3f
Olatunji.Sikiru <Olatunji.Sikiru@WN-HQ-LT-18>
2025-06-08 22:45:21 +0200

    [Olatunji] middler vowel filter tests

---
+++ TestProcessorSolution.ConsoleApp/FilterRules/MiddleVowelFilter.cs
@@ -0,0 +1,12 @@
+﻿namespace TestProcessorSolution.ConsoleApp.FilterRules
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	internal class MiddleVowelFilter
+	{
+	}
+}
---
+++ TestProcessorSolution.Tests/FilterRulesTest/MiddleVowelFilterTests.cs
@@ -0,0 +1,23 @@
+﻿namespace TestProcessorSolution.Tests.FilterRulesTest;
+
+	
+	public class MiddleVowelFilterTests
+	{
+		private readonly MiddleVowelFilter _filter = new();
+
+		[Theory]
+		[InlineData("clean", true)]
+		[InlineData("what", true)]
+		[InlineData("currently", true)]
+		[InlineData("the", false)]
+		[InlineData("rather", false)]
+		[InlineData("cry", false)]
+		public void ShouldRemove_MiddleVowel(string word, bool expected)
+		{
+			var result = _filter.ShouldRemove(word);
+			Assert.Equal(expected, result);
+		}
+	}
+
+
+
